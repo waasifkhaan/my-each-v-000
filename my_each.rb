53e2837c1b63a511f6464afd772f49e6dc679081
Wasif Khan <waasif.khaan@gmail.com>
2018-05-07 20:46:39 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
-end
+def my_each(array)
+  if block_given?
+    i = 0 
+      while i < array.length 
+        yield array[i]
+          i += 1 
+      end
+    array
+  else
+    puts " provide block to complete method"
+  end    
+ end
